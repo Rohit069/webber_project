d45873fb6fbc668484ccdc338168cc393a7bee54
RohitThakor <thakorrohit885@gmail.com>
2021-01-27 09:41:50 +0530

    add update files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 //import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {HttpClientModule} from '@angular/common/http';
 import {ReactiveFormsModule,FormsModule} from '@angular/forms';
+import { Page404Component } from './page404/page404.component';
 //import '@fortawesome/fontawesome-free';
 
 //import { FontAwesomeModule } from '@fortawesome/fontawesome-free';
@@ -13,6 +14,7 @@ import {ReactiveFormsModule,FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
+    Page404Component,
   ],
   imports: [
     BrowserModule,
